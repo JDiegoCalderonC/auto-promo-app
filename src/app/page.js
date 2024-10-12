a1841690c3da423dd1b48a879e4bbf4b3ee63b73
Diego <jdiegocalderon04@hotmail.com>
2024-10-12 10:26:31 -0500

    navbar and structure form

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@
 
 import Image from "next/image";
 import { useState } from "react";
+import logo from '../../public/images/logo.jpg';
+
 
 export default function Home() {
   const [formData, setFormData] = useState({
@@ -26,6 +28,7 @@ export default function Home() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
     // Lógica para generar un código alfanumérico
     const codigo = Math.random().toString(36).substr(2, 8).toUpperCase(); // Código aleatorio
     setCodigoGenerado(codigo);
@@ -44,8 +47,17 @@ export default function Home() {
   return (
     <div className="w-full h-full">
       
-      <nav className="h-1/10 text-3xl text-blue-600 font-bold font-serif py-2">
-        AutoBogotá
+      <nav className="h-1/10 p-1 flex items-center">
+      
+        <Image 
+          src={logo}
+          alt="Logo AutoPromo"
+          width={70} 
+          height={70}
+        />
+        <h1 className="text-4xl text-indigo-700 font-bold font-serif ml-3">AutoPromo</h1>
+        <h1 className="text-base text-black font-bold font-serif ml-1 mt-2 bg-yellow-400 px-2">Bogotá</h1>
+
       </nav>
 
       <div className="h-1/10 border border-neutral-300 py-2 mb-4">Iconos</div>
@@ -55,57 +67,64 @@ export default function Home() {
 
           <form
             onSubmit={handleSubmit}
-            className="w-3/5 h-full text-lg p-5 space-y-2 border border-neutral-300 rounded-lg"
+            className="w-3/5 h-full text-lg p-5 space-y-4 border border-neutral-300 rounded-lg"
           >
 
-            <h1 className="text-xl font-semibold text-center my-3">
+            <h1 className="text-2xl font-semibold text-center mt-2">
               Participa en Nuestro Concurso!
             </h1>
 
+            <div className="flex items-center justify-center">
+              <div className="w-2/3 border-b border-neutral-600 mb-2"></div>
+            </div>
+
             <div className="flex flex-col items-center">
-              <label className="block w-1/2">Nombre:</label>
+              <label className="block w-1/2 mb-1">Nombre*</label>
               <input
                 type="text"
                 name="nombre"
                 value={formData.nombre}
                 onChange={handleChange}
                 required
-                className="w-1/2"
+                className="w-1/2 border border-gray-400 rounded-lg p-1"
+                placeholder="Nombre"
               />
             </div>
 
             <div className="flex flex-col items-center">
-              <label className="block w-1/2">Apellido:</label>
+              <label className="block w-1/2 mb-1">Apellido*</label>
               <input
                 type="text"
                 name="apellido"
                 value={formData.apellido}
                 onChange={handleChange}
                 required
-                className="w-1/2"
+                className="w-1/2 border border-gray-400 rounded-lg p-1"
+                placeholder="Apellido"
               />
             </div>
 
             <div className="flex flex-col items-center">
-              <label className="block w-1/2">Cédula:</label>
+              <label className="block w-1/2 mb-1">Cédula*</label>
               <input
                 type="number"
                 name="cedula"
                 value={formData.cedula}
                 onChange={handleChange}
                 required
-                className="w-1/2"
+                className="w-1/2 border border-gray-400 rounded-lg p-1"
+                placeholder="Cédula de Ciudadania"
               />
             </div>
 
             <div className="flex flex-col items-center">
-              <label className="block w-1/2">Departamento:</label>
+              <label className="block w-1/2 mb-1">Departamento*</label>
               <select
                 name="departamento"
                 value={formData.departamento}
                 onChange={handleChange}
                 required
-                className="w-1/2"
+                className="w-1/2 border border-gray-400 rounded-lg p-1"
               >
                 <option value="">Selecciona</option>
                 <option value="Cundinamarca">Cundinamarca</option>
@@ -115,13 +134,13 @@ export default function Home() {
             </div>
 
             <div className="flex flex-col items-center">
-              <label className="block w-1/2">Ciudad:</label>
+              <label className="block w-1/2 mb-1">Ciudad*</label>
               <select
                 name="ciudad"
                 value={formData.ciudad}
                 onChange={handleChange}
                 required
-                className="w-1/2"
+                className="w-1/2 border border-gray-400 rounded-lg p-1"
               >
                 <option value="">Selecciona</option>
                 <option value="Bogotá">Bogotá</option>
@@ -131,26 +150,28 @@ export default function Home() {
             </div>
 
             <div className="flex flex-col items-center">
-              <label className="block w-1/2">Celular:</label>
+              <label className="block w-1/2 mb-1">Celular*</label>
               <input
                 type="number"
                 name="celular"
                 value={formData.celular}
                 onChange={handleChange}
                 required
-                className="w-1/2"
+                className="w-1/2 border border-gray-400 rounded-lg p-1"
+                placeholder="Teléfono"
               />
             </div>
 
             <div className="flex flex-col items-center">
-              <label className="block w-1/2">Correo Electrónico:</label>
+              <label className="block w-1/2 mb-1">Correo Electrónico*</label>
               <input
                 type="email"
                 name="email"
                 value={formData.email}
                 onChange={handleChange}
                 required
-                className="w-1/2"
+                className="w-1/2 border border-gray-400 rounded-lg p-1"
+                placeholder="email@address.com"
               />
             </div>
 
@@ -161,11 +182,10 @@ export default function Home() {
                 checked={formData.autorizado}
                 onChange={handleChange}
                 required
-                className="w-5 h-5 mr-2"
+                className="w-5 h-5 mr-4"
               />
-              <label className="">
-                Autorizo el tratamiento de mis datos de acuerdo a la política de
-                protección de datos.
+              <label className="max-w-3xl">
+                Autorizo el tratamiento de mis datos de acuerdo con la finalidad establecida en la política de protección de datos personales.
               </label>
             </div>
 
